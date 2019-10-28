fe40b0724f5319ed713c6cd667315b717041a14f
zhongyuCN <531939845@qq.com>
2019-10-28 23:11:59 +0800

    UpLoad
    上传

---
+++ WebAppDemo/Controllers/ContentController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppDemo.Models;
+
+namespace WebAppDemo.Controllers
+{
+    public class ContentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var contents = ReadTxt(@"D:\DataBase.txt");
+            //for(int i = 0; i < 10; i++)
+            //{
+            //    contents.Add(new Content() { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            //}
+            return View(new ContentViewModel { Contents=contents});
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string queryString)
+        {
+            string id= Convert.ToString(Request.Form["Id"]);
+            string title= Convert.ToString(Request.Form["title"]);
+            string content = Convert.ToString(Request.Form["content"]);
+            string status = Convert.ToString(Request.Form["status"]);
+            string addDate = Convert.ToString(Request.Form["add_time"]);
+
+            AppendTxt($"{id} {title} {content} {status} {addDate}", @"D:\DataBase.txt");
+
+            return View("Index");
+        }
+
+        private List<Content> ReadTxt(string path)
+        {
+            List<Content> list = new List<Content>();
+             
+
+            using (TextReader reader=new StreamReader(path))
+            {
+                string line = string.Empty;
+                Content content = new Content();
+                
+                while (!string.IsNullOrEmpty( line = reader.ReadLine()))
+                {
+                   string[] array= line.Split(' ');
+                    if (array.Length < 5)
+                    {
+                        continue;
+                    }
+                   
+                    content.Id =Convert.ToInt32( array[0]);
+                    content.title = array[1];
+                    content.content = array[2];
+                    content.status= Convert.ToInt32(array[3]);
+                    content.add_time =DateTime.Parse( array[4]);
+                    list.Add(content);
+                }
+               
+            }
+                return list;
+        }
+
+        private void AppendTxt(string text,string path)
+        {
+            using (TextWriter writer = new StreamWriter(path,true))
+            {
+                writer.WriteLine(text);
+            }
+        }
+
+    }
+}
---
+++ WebAppDemo/Models/ContentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppDemo.Models
+{
+    public class ContentViewModel
+    {
+        
+            /// <summary>
+            /// 内容列表
+            /// </summary>
+            public List<Content> Contents { get; set; }
+        
+    }
+}
